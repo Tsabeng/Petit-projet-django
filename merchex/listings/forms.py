2c7dd2abc386f6e023964e6aaf3d336a2a5c7fb8
dtsabeng <delphantsabeng@yahoo.com>
2025-07-21 16:07:58 +0200

    Days 3

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,6 +1,19 @@
 from django import forms 
+from listings.models import Band,Listing
 
 class ContactUsForm(forms.Form):
     name=forms.CharField(required=False)
     email=forms.EmailField()
     message=forms.CharField(max_length=1000)
+
+class BandForm(forms.ModelForm):
+    class Meta:
+        model=Band
+        exclude =('active','official_homepage') # utiliser exclude signifie que l'utilisateur  aurait les champ du formulaire sauf active et official_homepage ils pourront seulement etre modifier par l'administrateur
+       # fields='__all__' # mettre ca signifie que l'on prend tout les champs du models
+class ListingForm(forms.ModelForm):
+
+    class Meta:
+        model=Listing
+        exclude = ('sold',)
+        #fields='__all__'
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 
 from listings.models import Band,Listing
 
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm,BandForm,ListingForm
 from django.core.mail import send_mail
-
 from django.shortcuts import redirect
 
 def band_list (request):
@@ -17,6 +16,42 @@ def band_detail(request,id):
     return render(request,
                   'listings/band_detail.html',
                   {'band':band})
+
+def band_create(request):
+
+    if request.method == 'POST':
+
+        form = BandForm(request.POST)
+
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+
+        form = BandForm()
+    return render(request,'listings/band_create.html',{'form': form})
+
+def band_change(request,id):
+    band =Band.objects.get(id=id) 
+    if request.method == 'POST':
+
+        form = BandForm(request.POST, instance=band)
+
+        if form.is_valid():
+            form.save() # mettre à jour le groupe existant dans la base de donnees
+            return redirect('band-detail', band.id) # rediriger vers la page détaillée du groupe que nous venons de mettre a jour
+
+    else:
+        form = BandForm(instance=band) 
+    return render(request,'listings/band_change.html',{'form':form})
+def band_delete(request,id):
+    band=Band.objects.get(id=id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    return render(request,'listings/band_delete.html',{'band':band})
+
+
 def about(request):
     return render(request,'listings/about.html')
 
@@ -29,6 +64,35 @@ def listing_detail(request,id):
     return render(request,
                   'listings/listing_detail.html',
                   {'listing':listing})
+def listing_create(request):
+     if request.method == 'POST':
+        form = ListingForm(request.POST)
+
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+     else:
+
+        form = ListingForm()
+     return render(request,'listings/listing_create.html',{'form': form})
+
+def listing_change(request,id):
+    listing =Listing.objects.get(id=id) 
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save() # mettre à jour l'annonce existant dans la base de donnees
+            return redirect('listing-detail', listing.id) # rediriger vers la page détaillée de l'annonce que nous venons de mettre a jour
+    else:
+        form = ListingForm(instance=listing) 
+    return render(request,'listings/listing_change.html',{'form':form})
+
+def listing_delete(request,id):
+    listing=Listing.objects.get(id=id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    return render(request,'listings/listing_delete.html',{'listing':listing})
 
 def contact(request):
     
